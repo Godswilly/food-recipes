85ea42b5d791765dc4c0c6e1a27254f32b0c8416
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-05-25 09:44:27 +0100

    Update MealFilter to handle category change

--- src/components/MealFilter.js
+++ src/components/MealFilter.js
@@ -1,18 +1,27 @@
 import React from 'react';
 
 const MealFilter = (props) => {
+  const { handleChange, category } = props;
   return (
-    <div>
-      <select onChange={props.handlechange}>
-        <option value="all" key="all">
+    <div className="filtered">
+      <h3>Choose a Category</h3>
+      <select className="filter" onChange={handleChange}>
+        <option value="All" key="All">
           {' '}
-          ---{' '}
+          ---
+          {' '}
         </option>
-        {props.category.map((cat) => (
-          <option key={cat.strCategory}>{cat.strCategory}</option>
+        {category.map((caty) => (
+          <option data-testid={caty.strCategory} key={caty.strCategory}>{caty.strCategory}</option>
         ))}
       </select>
     </div>
   );
 };
+
+MealFilter.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  category: PropTypes.array.isRequired,
+};
+
 export default MealFilter;
