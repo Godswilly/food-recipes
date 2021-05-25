2404476f045587824af50b3f7e2116c742e71b3a
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-05-25 09:59:47 +0100

    Update MealCard to accept propTypes

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const MealCard = (props) => {
+  const {
+    meal, image, id,
+  } = props;
+
   return (
     <div>
-      <img src={props.img} />
-      <h1> {props.meal} </h1>
+      <Link to={`/meal/${id}/${meal}`}>
+        <div data-testid="div" className="mealcard">
+          <img data-testid="image" alt={meal} src={image} />
+          <h3 data-testid="meal">
+            {' '}
+            {meal}
+            {' '}
+          </h3>
+        </div>
+      </Link>
     </div>
   );
 };
+
+MealCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  meal: PropTypes.string.isRequired,
+};
+
 export default MealCard;
